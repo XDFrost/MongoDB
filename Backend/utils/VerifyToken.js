43e107a3ee9029543b7f131f3caa1b503ffd0681
Pratham Gupta <pratham0gupta@gmail.com>
2024-11-22 09:57:39 +0530

    Remove outdated MongoDB notes and clean up token verification logic

--- Backend/utils/VerifyToken.js
+++ Backend/utils/VerifyToken.js
@@ -5,7 +5,7 @@ export const verifyToken = (req, res, next) => {
     // console.log(token);
 
     const token = req.headers["authorization"]?.split(" ")[1]; // Get the token part after "Bearer"
-
+    
     if (!token) {
         return res.status(401).send("Access Denied");
     }
